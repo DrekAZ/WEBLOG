fcfcec8d0713ded029941d56768866e6470b7507
DrekAZ <kassuruv2@gmail.com>
2024-11-02 14:20:19 +0900

    first commit

---
+++ app/routes/test.t2.md
@@ -0,0 +1,10 @@
+---
+meta:
+  - title: My S Post
+  - name: description
+    content: Isn't this awesome?
+headers:
+  Cache-Control: no-cache
+---
+
+# Hello Content2
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import mdx from '@mdx-js/rollup'
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import { copyFileSync } from 'node:fs'
+import { join } from 'node:path'
 
 declare module "@remix-run/node" {
   interface Future {
@@ -10,6 +15,10 @@ declare module "@remix-run/node" {
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
+    mdx({
+       remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+    }),
     remix({
       future: {
         v3_fetcherPersist: true,
@@ -18,7 +27,19 @@ export default defineConfig({
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
       },
+
+      ssr: false,
+      basename: '/weblog/',
+      buildEnd(args) {
+        if(!args.viteConfig.isProduction) return
+        const buildPath = args.viteConfig.build.outDir
+        copyFileSync(
+          join(buildPath, 'index.html'),
+          join(buildPath, '404.html')
+        )
+      }
     }),
-    tsconfigPaths(),
   ],
+  
+  base: '/weblog/',
 });
